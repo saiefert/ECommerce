265e82edcbea5914375544329a6940e4f4bb41ce
saiefert <josmar.saiefert@outlook.com>
2020-07-12 00:14:23 -0400

    Adicionar arquivos de projeto.

---
+++ Aulas/Aula2/CasaDoCodigo/DataService.cs
@@ -0,0 +1,41 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using static CasaDoCodigo.Repositories.ProdutoRepository;
+
+namespace CasaDoCodigo
+{
+    public partial class Startup
+    {
+        public class DataService : IDataService
+        {
+            private readonly ApplicationContext context;
+            private readonly IProdutoRepository produtoRepository;
+
+            public DataService(ApplicationContext context,
+                IProdutoRepository produtoRepository)
+            {
+                this.context = context;
+                this.produtoRepository = produtoRepository;
+            }
+
+            public void InicializaDB()
+            {
+                context.Database.EnsureCreated();
+
+                List<Livro> livros = GetLivros();
+
+                produtoRepository.SaveProdutos(livros);
+            }
+
+            private static List<Livro> GetLivros()
+            {
+                var json = File.ReadAllText("livros.json");
+                var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+                return livros;
+            }
+        }        
+    }
+}
